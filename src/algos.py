88db7a8cfe729e6773e4df9ea1858ce5de91504c
wynnson <wynnsondaone@gmail.com>
2026-02-14 08:43:50 -0500

    add dfs, add control for bfs, dfs

--- src/algos.py
+++ src/algos.py
@@ -3,24 +3,24 @@ from collections import deque
 from src.grid import Grid
 from src.config.colors import Colors
 
-def bfs(grid: Grid, color: tuple[int], x: int=0, y: int=0) -> bool:
+def bfs(grid: Grid, color: tuple[int], r: int=0, c: int=0) -> bool:
     """BFS traveler.
 
     Args:
         grid: grid world
         color: color of traveler
-        x: start x
-        y: start y
+        c: start r
+        r: start c
     """
     clock = pygame.time.Clock()
     completed = False
     
     neighbors = deque()
     visited = []
-    neighbors.append((x, y))
-    visited.append((x, y))
+    neighbors.append((r, c))
+    visited.append((r, c))
 
-    grid.values[x][y] = 0
+    grid.values[r][c] = 0
 
     while neighbors:
         for event in pygame.event.get():
@@ -46,22 +46,82 @@ def bfs(grid: Grid, color: tuple[int], x: int=0, y: int=0) -> bool:
             break
 
         directions = ((r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1))
-        for neighbor_row, neighbor_col in directions:
-            if 0 <= neighbor_row < grid.num_rows and 0 <= neighbor_col < grid.num_cols:
+        for dx, dy in directions:
+            if 0 <= dx < grid.num_rows and 0 <= dy < grid.num_cols:
 
-                if grid.values[neighbor_row][neighbor_col] == 1:
-                    grid.values[neighbor_row][neighbor_col] = -1
-                    neighbors.append((neighbor_row, neighbor_col))
+                if grid.values[dx][dy] == 1:
+                    grid.values[dx][dy] = -1
+                    neighbors.append((dx, dy))
 
-                elif grid.values[neighbor_row][neighbor_col] == 2:
-                    neighbors.append((neighbor_row, neighbor_col))
+                elif grid.values[dx][dy] == 2:
+                    neighbors.append((dx, dy))
 
     status = Colors.GREEN if completed else Colors.RED
-    bfs_trace(grid, visited, status)
+    trace(grid, visited, status)
     return False
-        
-def bfs_trace(grid: Grid, visited: set, color: tuple[int]) -> None:
-    """BFS tracer to highlight traveler.
+
+
+def dfs(grid: Grid, color: tuple[int], r: int=0, c: int=0) -> bool:
+    """DFS traveler.
+
+    Args:
+        grid: grid world
+        color: color of traveler
+        r: start x
+        c: start y
+    """
+    clock = pygame.time.Clock()
+    completed = False
+    visited = []
+
+    if grid.values[r][c] == 0 or grid.values[r][c] == -1:
+        return False
+
+    stack = [(r, c)]
+
+    if grid.values[r][c] == 1:
+        grid.values[r][c] = -1
+
+    while stack:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                raise SystemExit
+
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+                return True
+
+        pygame.event.pump()
+        clock.tick(80)
+
+        curr_r, curr_c = stack.pop()
+        cell = grid.values[curr_r][curr_c]
+
+        rect = pygame.Rect(curr_c * grid.cell_size, curr_r * grid.cell_size, grid.cell_size, grid.cell_size)
+        pygame.draw.rect(grid.screen, color, rect)
+        pygame.display.update(rect)
+        visited.append((curr_r, curr_c))
+
+        if cell == 2:
+            completed = True
+            break
+    
+        directions = ((curr_r + 1, curr_c), (curr_r - 1, curr_c), (curr_r, curr_c + 1), (curr_r, curr_c - 1))
+        for dx, dy in directions:
+            if 0 <= dx < grid.num_rows and 0 <= dy < grid.num_cols:
+                if grid.values[dx][dy] == 1:
+                    grid.values[dx][dy] = -1
+                    stack.append((dx, dy))
+                elif grid.values[dx][dy] == 2:
+                    stack.append((dx, dy))
+    
+    status = Colors.GREEN if completed else Colors.RED
+    trace(grid, visited, status)
+    return False
+
+
+def trace(grid: Grid, visited: set, color: tuple[int]) -> None:
+    """Tracer to highlight traveler.
     
     Args:
         grid: grid world. 
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import random
 from src.config.colors import Colors
 from src.config.display import Display
 from src.grid import Grid
-from src.algos import bfs
+from src.algos import bfs, dfs
 
 pygame.init()
 screen = pygame.display.set_mode((Display.SCREEN_WIDTH, Display.SCREEN_HEIGHT))
@@ -33,23 +33,29 @@ try:
                     grid.reset()
                     has_run = False
 
-        pygame.display.update()
+                pygame.display.update()
 
-        if not has_run:
-            invalid = True
-            while invalid:
-                start_x = random.randint(0, grid.num_rows - 1)
-                start_y = random.randint(0, grid.num_cols - 1)
-                starting_position = grid.values[start_x][start_y]
-                invalid = (starting_position == 1 or starting_position == 2)
+                if not has_run and (event.key in (pygame.K_d, pygame.K_b)):
+                    invalid = True
+                    while invalid:
+                        start_r = random.randint(0, grid.num_rows - 1)
+                        start_c = random.randint(0, grid.num_cols - 1)
+                        starting_position = grid.values[start_r][start_c]
+                        invalid = (starting_position == 0 or starting_position == 2)
+                    
+                    if event.key == pygame.K_d:
+                        run_again = dfs(grid, color=Colors.YELLOW, r=start_r, c=start_c)
+                    
+                    if event.key == pygame.K_b:
+                        run_again = bfs(grid, color=Colors.YELLOW, r=start_r, c=start_c)
 
-            run_again = bfs(grid, x=start_x, y=start_y, color=Colors.YELLOW)
+                    if run_again:
+                        grid.reset()
+                        has_run = False
+                    else:
+                        has_run = True
 
-            if run_again:
-                grid.reset()
-                has_run = False
-            else:
-                has_run = True
+            pygame.display.update()
 
 except KeyboardInterrupt:
     print("Exited successfully")
